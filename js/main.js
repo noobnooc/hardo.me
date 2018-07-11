5dc258fe0762138eec92ef305ca3543d9c26475e
Hardo <i@hardo.me>
2018-07-11 15:06:00 +0800

    添加社会主义核心价值观

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
-document.body.addEventListener('click', function (mouseEvent) {
-  document.body.style.position = 'relative';
-  generateBubble(mouseEvent.clientX, mouseEvent.clientY);
-}, true)
+window.onload = function() {
+  document.body.addEventListener('click', function (mouseEvent) {
+    document.body.style.position = 'relative';
+    generateBubble(mouseEvent.clientX, mouseEvent.clientY);
+  }, true)
+}
+
+const textList = ['富强', '民主', '文明', '和谐', '自由', '平等', '公正', '法治', '爱国', '敬业', '诚信', '友善']
 
 function generateBubble(x, y) {
-  var bubbleImg = document.createElement('img');
-  bubbleImg.className = 'bubble-img';
-  bubbleImg.src = 'images/fav.png';
-  bubbleImg.style.width = '20px';
-  bubbleImg.style.height = '20px';
-  bubbleImg.style.position = 'absolute';
-  bubbleImg.style.top = y - 10 + 'px';
-  bubbleImg.style.left = x - 10 + 'px';
-  bubbleImg.style.opacity = '0.05';
-  document.body.appendChild(bubbleImg);
+  let bubble = document.createElement('span');
+  let text = textList[Math.floor(Math.random() * textList.length)];
+  bubble.innerText = text;
+  bubble.className = 'bubble';
+  bubble.style.position = 'absolute';
+
+  document.body.appendChild(bubble);
+  
+  bubble.style.top = y - (bubble.offsetHeight / 2) + 'px';
+  bubble.style.left = x - (bubble.offsetWidth / 2) + 'px';
+  setTimeout(() => document.body.removeChild(bubble), 900);
+
 }
