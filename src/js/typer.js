2f3a3ac1be7663a357f0eacdf66bdac7536f3e9c
Hardo <i@hardo.me>
2018-07-22 17:57:12 +0800

    添加代码块的输入输出区别

--- src/js/typer.js
+++ src/js/typer.js
@@ -4,7 +4,7 @@ export default {
     this.codeBoxEle = document.querySelector(el);
     this.codeList = codeList;
     this.lineEle = null;
-    this.typedLine = '';
+    this.typedLine = "";
     this.cursor = false;
 
     this.started = false;
@@ -18,14 +18,14 @@ export default {
   async _startType() {
     this._blinkCursor();
     for (let line of this.codeList) {
-      let lineEle = document.createElement('p');
-      lineEle.className = 'code-line';
-      this.codeBoxEle.append(lineEle);
-      this.cursor = false;
-      this._print();
-      this.lineEle = lineEle;
-      this.typedLine = '';
-      await this._typeLine(line);
+      let input = line.split(' ')[0] === '>';
+      if (input) {
+        line = line.split(' ').slice(1).join(' ');
+        await this._typeInput(line);
+      } else {
+        this._printOutput(line);
+      }
+      
     }
   },
 
@@ -43,18 +43,36 @@ export default {
   },
 
   _type(char) {
-    return new Promise((resolve) => {
+    return new Promise(resolve => {
       const timeout = Math.floor(Math.random() * 50 + 50);
       setTimeout(() => {
         this.typedLine += char;
         this._print();
         resolve();
       }, timeout);
-    })
+    });
   },
 
   _print() {
     if (!this.lineEle) return;
-    this.lineEle.innerText = this.typedLine + (this.cursor ? '_' : '');
+    this.lineEle.innerText = this.typedLine + (this.cursor ? "_" : "");
+  },
+
+  _printOutput(line) {
+    let ele = document.createElement("p");
+    ele.className = "code-line";
+    ele.innerText = line;
+    this.codeBoxEle.append(ele);
+  },
+
+  async _typeInput(line) {
+    let lineEle = document.createElement("p");
+    lineEle.className = 'code-line input-line';
+    this.codeBoxEle.append(lineEle);
+    this.cursor = false;
+    this._print();
+    this.lineEle = lineEle;
+    this.typedLine = '';
+    await this._typeLine(line);
   }
-}
+};
